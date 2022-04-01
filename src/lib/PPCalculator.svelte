b3a4f0cdbc89567f21c89af97a9d936a94d823d7
Shurdoof <me@shurdoof.com>
2022-04-01 23:23:06 +0200

    Lots of improvements

--- src/lib/PPCalculator.svelte
+++ src/lib/PPCalculator.svelte
@@ -8,18 +8,66 @@
 	let curve: Curve = curves[0];
 </script>
 
-<div>
-	<select bind:value={curve}>
+<div class="p-4">
+	<select bind:value={curve} class="select select-bordered">
 		{#each curves as curve}
 			<option value={curve}>
 				{curve.name}
 			</option>
 		{/each}
 	</select>
+	<div class="divider" />
 
-	<input type="text" bind:value={stars} />
-	<input type="text" bind:value={acc} />
-	<div>{calculatePP(curve.points, stars, acc).pp.toFixed(2)}</div>
+
+	<div class="stats bg-base-200">
+		<div class="stat">
+			<div class="stat-figure text-primary" />
+			<div class="stat-title">Accuracy %</div>
+
+			<div class="stat-value">
+				<input type="number" bind:value={acc} class="input input-bordered input-sm" min="0" max="100" />
+			</div>
+		</div>
+
+		<div class="stat">
+			<div class="stat-figure text-primary" />
+			<div class="stat-title">Star rating ★</div>
+
+			<div class="stat-value">
+				<input type="number" bind:value={stars} class="input input-bordered input-sm" min="0" max="100" />
+			</div>
+		</div>
+
+		<div class="stat">
+			<div class="stat-figure text-primary" />
+			<div class="stat-title">Performance Points</div>
+
+			<div class="stat-value text-primary">
+				{calculatePP(curve.points, stars, acc).pp.toFixed(2)}
+			</div>
+		</div>
+	</div>
+
+	<!-- <div class="form-control w-full max-w-xs">
+		<span class="label">
+			<span class="label-text">Accuracy (0-100)</span>
+		</span>
+		<input type="number" bind:value={acc} class="input input-primary input-sm" min="0" max="100" />
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<span class="label">
+			<span class="label-text">Star rating</span>
+		</span>
+		<input type="text" bind:value={stars} class="input input-bordered input-sm w-full max-w-xs" />
+	</div>
+
+	<input type="text" bind:value={stars} class="input input-primary input-sm" />
+	<input type="text" bind:value={acc} class="input input-primary input-sm" />
+	<span> = </span>
+	<input type="text" disabled value={calculatePP(curve.points, stars, acc).pp.toFixed(2)} class="input input-primary input-sm" /> -->
+
+	<div class="divider" />
 
 	<PPMatrix {curve} />
 </div>
--- src/lib/PPMatrix.svelte
+++ src/lib/PPMatrix.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { calculatePP } from '../pp/calculator';
 	import { curves, type Curve } from '../pp/curves';
+	type HoverState = { star: number; acc: number };
+	type HighlightType = 'cell' | 'row/column' | 'none';
 
 	function range(start: number, end: number) {
 		return Array.from(new Array(end - start + 1), (x, i) => i + start);
@@ -13,29 +15,59 @@
 
 	export let minAcc = 85;
 	export let maxAcc = 100;
+
+	let currentHover: HoverState | null = null;
 </script>
 
 <div>
-	<table>
+	<table class="table table-compact pp-matrix">
 		<thead>
 			<tr>
 				<th />
 				{#each range(minStars, maxStars) as star}
-					<th>{star}★</th>
+					<th class:highlight={star === currentHover?.star}>{star} ★</th>
 				{/each}
 			</tr>
 		</thead>
 		<tbody>
 			{#each range(minAcc, maxAcc) as acc}
 				<tr>
-					<th>
+					<th class:highlight={acc === currentHover?.acc}>
 						{acc}%
 					</th>
 					{#each range(minStars, maxStars) as star}
-						<td>{calculatePP(curve.points, star, acc).pp.toFixed(2)}</td>
+						<td
+							class:hover={star === currentHover?.star && acc === currentHover?.acc}
+							class:highlight={star === currentHover?.star || acc === currentHover?.acc}
+							on:mouseenter={e => (currentHover = { star, acc })}
+							on:mouseleave={e => (currentHover = null)}>{calculatePP(curve.points, star, acc).pp.toFixed(2)}</td
+						>
 					{/each}
 				</tr>
 			{/each}
 		</tbody>
 	</table>
 </div>
+
+<style>
+	.pp-matrix {
+		table-layout: fixed;
+	}
+	.pp-matrix td,
+	.pp-matrix th {
+		width: 70px;
+		text-align: right;
+	}
+	.highlight {
+		@apply bg-base-200;
+	}
+
+	th.highlight {
+		@apply text-info;
+	}
+
+	.hover {
+		@apply bg-base-300;
+		@apply text-info;
+	}
+</style>
