2c39def624957e18b4362b0a89c2c4f2a5d86db4
Shurdoof <me@shurdoof.com>
2023-05-14 17:23:49 +0200

    Add autogenV3 curve

--- src/lib/pp/curves.ts
+++ src/lib/pp/curves.ts
@@ -156,7 +156,7 @@ const AutogenV1Curve: Curve = {
 
 let AutogenV2Curve: Curve = {
     id: 'autogenv2',
-    name: 'Automatically Generated Curve V2 (next?)',
+    name: 'Automatically Generated Curve V2',
     points: [
         [1, 6.114501330729609],
         [0.999, 5.160678759790276],
@@ -195,6 +195,50 @@ let AutogenV2Curve: Curve = {
     ]
 };
 
+let AutogenV3Curve: Curve = {
+    id: 'autogenv3',
+    name: 'Automatically Generated Curve V3 (next?)',
+    points: [
+        [1, 5.367394282890631],
+        [0.9995, 5.019543595874787],
+        [0.999, 4.715470646416203],
+        [0.99825, 4.325027383589547],
+        [0.9975, 3.996793606763322],
+        [0.99625, 3.5526145337555373],
+        [0.995, 3.2022017597337955],
+        [0.99375, 2.9190155639254955],
+        [0.9925, 2.685667856592722],
+        [0.99125, 2.4902905794106913],
+        [0.99, 2.324506282149922],
+        [0.9875, 2.058947159052738],
+        [0.985, 1.8563887693647105],
+        [0.9825, 1.697536248647543],
+        [0.98, 1.5702410055532239],
+        [0.9775, 1.4664726399289512],
+        [0.975, 1.3807102743105126],
+        [0.9725, 1.3090333065057616],
+        [0.97, 1.2485807759957321],
+        [0.965, 1.1552120359501035],
+        [0.96, 1.0871883573850478],
+        [0.955, 1.0388633331418984],
+        [0.95, 1],
+        [0.94, 0.9417362980580238],
+        [0.93, 0.9039994071865736],
+        [0.92, 0.8728710341448851],
+        [0.91, 0.8488375988124467],
+        [0.9, 0.825756123560842],
+        [0.875, 0.7816934560296046],
+        [0.85, 0.7462290664143185],
+        [0.825, 0.7150465663454271],
+        [0.8, 0.6872268862950283],
+        [0.75, 0.6451808210101443],
+        [0.7, 0.6125565959114954],
+        [0.65, 0.5866010012767576],
+        [0.6, 0.18223233667439062],
+        [0, 0]
+    ]
+};
+
 export function getCurveById(id: string) {
     return curves.find(x => x.id === id);
 }
@@ -224,4 +268,4 @@ export function getInitialCurve(value: string | undefined, defaultCurve = curves
     return getCurveById(value) || parseQueryStringCurve(value) || defaultCurve;
 }
 
-export const curves = [duhhelRamenV6Curve, AutogenV2Curve, AutogenV1Curve, duhhelRamenV5Curve, fernCurve];
+export const curves = [duhhelRamenV6Curve, AutogenV3Curve, AutogenV2Curve, AutogenV1Curve, duhhelRamenV5Curve, fernCurve];
